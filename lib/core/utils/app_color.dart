ddf87de8c3215ac7b5cddba9a28deb85633028f9
reda ahmed <43178331+username@users.noreply.github.com>
2023-04-24 12:24:36 +0200

    Creating App Colors

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const blackColor = Color(0xff000000);
+  static const lightBlackColor = Color(0xff3F3F3F);
+
+  static const greyColor = Color(0xffD4D4D4);
+  static const darkGreyColor = Color(0xff464646);
+
+  static const purpleColor = Color(0xff22215B);
+
+  static const blueColor = Color(0xff567DF4);
+  static const yellowColor = Color(0xffF3C939);
+  static const redColor = Color(0xffF45656);
+  static const greenColor = Color(0xff34DEDE);
+
+  static const lightBlueColor = Color(0xffEEF7FE);
+  static const lightYellowColor = Color(0xffFFFBEC);
+  static const lightRedColor = Color(0xffFEEEEE);
+  static const lightGreenColor = Color(0xffF0FFFF);
+}
