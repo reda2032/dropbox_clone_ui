bb19d8630a24ad2958d08f1a6aae28eb53ae95b9
reda ahmed <43178331+username@users.noreply.github.com>
2023-04-24 13:31:55 +0200

    Creating App Fonts

---
+++ lib/core/utils/app_font.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppFontConstants {
+  static const String fontFamily = "Gilroy";
+}
+
+class AppFontWeight {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class AppFontSize {
+  static const double s10 = 10.0;
+  static const double s12 = 12.0;
+  static const double s13 = 13.0;
+  static const double s14 = 14.0;
+  static const double s15 = 15.0;
+  static const double s16 = 16.0;
+  static const double s20 = 20.0;
+  static const double s24 = 24.0;
+  static const double s38 = 38.0;
+}
